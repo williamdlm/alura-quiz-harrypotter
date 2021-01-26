5e6bdfa20bcd930863861e77be25d8d357b9702b
WILLIAM <williammota.david@gmail.com>
2021-01-26 02:48:25 -0300

    metatags added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 import db from "../db.json";
 import Widget from "../src/components/Widget";
 import QuizBackground from "../src/components/QuizBackground";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
-
+import Head from "../src/components/Head";
 
 // const BackgroundImage = styled.div `
 // background-image: url(${db.bg});
@@ -24,34 +24,31 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head />
       <QuizContainer>
-        
         <Widget>
-            <Widget.Header><h1>Harry Potter</h1></Widget.Header>
-         <Widget.Content>
-  
+          <Widget.Header>
+            <h1>Harry Potter</h1>
+          </Widget.Header>
+          <Widget.Content>
             <p>Officia elit anim qui exercitation do in.</p>
-         </Widget.Content>
+          </Widget.Content>
         </Widget>
 
         <Widget>
-        <Widget.Content>
-          <h1>Harry Potter</h1>
-  
-  <p>Officia elit anim qui exercitation do in.</p>
-        </Widget.Content>
+          <Widget.Content>
+            <h1>Harry Potter</h1>
+
+            <p>Officia elit anim qui exercitation do in.</p>
+          </Widget.Content>
         </Widget>
 
-      <Footer />
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="#" />
+      <GitHubCorner projectUrl="https://github.com/williamdlm/alura-quiz-harrypotter" />
     </QuizBackground>
-
-  )
+  );
 }
-
---
+++ src/components/Head/index.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+function IndexPage() {
+  return (
+    <Head>
+      <title>Harry Potter Quiz</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      {/* Metatags conforme solicitado no desafio */}
+      {/* <!-- Primary Meta Tags --> */}
+      <meta name="title" content="Harry Potter Quiz" />
+      <meta name="description" content="" />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Harry Potter Quiz" />
+      <meta property="og:description" content="" />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/dhmkfekt2/image/upload/v1611644437/hogwarts-wallpaper-3_i2hfvo.jpg"
+      />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:title" content="Harry Potter Quiz" />
+      <meta property="twitter:description" content="" />
+      <meta
+        property="twitter:image"
+        content="https://res.cloudinary.com/dhmkfekt2/image/upload/v1611644437/hogwarts-wallpaper-3_i2hfvo.jpg"
+      />
+      <meta property="og:image:width" content="600" />
+    </Head>
+  );
+}
+
+export default IndexPage;
