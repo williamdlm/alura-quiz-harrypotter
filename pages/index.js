54dd5614392cc424555cc4862e650c8af9d77643
WILLIAM <williammota.david@gmail.com>
2021-01-26 01:51:50 -0300

    codigo ao final da primeira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,57 @@
 import styled from 'styled-components'
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+// const BackgroundImage = styled.div `
+// background-image: url(${db.bg});
+//     flex: auto;
+//     background-size: cover;
+//     background-position: center;
+// `
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        
+        <Widget>
+            <Widget.Header><h1>Harry Potter</h1></Widget.Header>
+         <Widget.Content>
+  
+            <p>Officia elit anim qui exercitation do in.</p>
+         </Widget.Content>
+        </Widget>
+
+        <Widget>
+        <Widget.Content>
+          <h1>Harry Potter</h1>
+  
+  <p>Officia elit anim qui exercitation do in.</p>
+        </Widget.Content>
+        </Widget>
+
+      <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="#" />
+    </QuizBackground>
+
+  )
 }
+
